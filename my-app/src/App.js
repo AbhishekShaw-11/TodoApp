0108d49003d5969d584d94f9df9c08f5b5708ca7
abhishek 1199 <abhishekshaw1199@gmail.com>
2023-12-15 13:40:05 +0530

    1

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './com/Home';
+
+function App() {
+  return (
+    <>
+    <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/com/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div className='main'>
+    Home
+    <div className='sec'>
+    input here
+    <div className='tag'>
+    <input type='text' className='place' placeholder='Add your daily tasks' />
+    
+    
+    </div>
+    <div className='btn'>
+    <button><span>ADD</span></button>
+    </div>
+    </div>
+    </div>
+
+    </>
+  )
+}
+
+export default Home
