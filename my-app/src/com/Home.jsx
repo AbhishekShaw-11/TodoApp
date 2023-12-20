887840509d76e39d29076954cbbe591a4cfc8eae
abhishek 1199 <abhishekshaw1199@gmail.com>
2023-12-20 13:44:24 +0530

    day2

--- my-app/src/com/Home.jsx
+++ my-app/src/com/Home.jsx
@@ -2,23 +2,24 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <>
-    <div className='main'>
-    Home
-    <div className='sec'>
-    input here
-    <div className='tag'>
-    <input type='text' className='place' placeholder='Add your daily tasks' />
-    
-    
-    </div>
-    <div className='btn'>
-    <button><span>ADD</span></button>
-    </div>
-    </div>
-    </div>
+    <React.Fragment>
+      <div className=" border-solid border-2 border-indigo-600  bg-gray-400  w-full h-96 ">
+        <div className='sec  justify-center m-20'>
+          <h1 className='text-3xl border-solid border-2 border-indigo-600  flex justify-center '>TODO</h1>
 
-    </>
+          
+          <div className='tag flex justify-center'>
+            <input type='text' className='place' placeholder='Add your daily tasks' />
+
+
+          </div>
+          <div className='btn flex justify-center'>
+            <button className=' hover:bg-white text:Remove All'><span>ADD</span></button>
+          </div>
+        </div>
+      </div>
+
+    </React.Fragment>
   )
 }
 
