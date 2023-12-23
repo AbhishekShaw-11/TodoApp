007f23152a5eaee6ec22d055aac851574f233d44
abhishek 1199 <abhishekshaw1199@gmail.com>
2023-12-23 19:57:59 +0530

    ui

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Home from './com/Home';
 
+
 function App() {
   return (
-    <>
+    <React.Fragment>
     <Home/>
-    </>
+    </React.Fragment>
   );
 }
 
--- my-app/src/com/Home.jsx
+++ my-app/src/com/Home.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
+import './Home.css';
 
 const Home = () => {
   return (
     <React.Fragment>
-      <div className=" border-solid border-2 border-indigo-600  bg-gray-400  w-full h-96 ">
-        <div className='sec  justify-center m-20'>
-          <h1 className='text-3xl border-solid border-2 border-indigo-600  flex justify-center '>TODO</h1>
-
-          
-          <div className='tag flex justify-center'>
-            <input type='text' className='place' placeholder='Add your daily tasks' />
-
-
-          </div>
-          <div className='btn flex justify-center'>
-            <button className=' hover:bg-white text:Remove All'><span>ADD</span></button>
+      <div className='Home-main'>
+        <div className='pre  bor'>
+          <h1>
+            Todo APP
+          </h1>
+          <input className='Home-inp' placeholder='Add your daily task'></input>
+          <div className='Home-showData'></div>
+          <div className='Home-btn'>
+            <button>+</button>
           </div>
         </div>
       </div>
