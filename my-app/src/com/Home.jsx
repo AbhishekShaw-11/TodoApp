b4c9c6df785ee33117fd19c41a3bd1696374637e
abhishek 1199 <abhishekshaw1199@gmail.com>
2023-12-24 20:39:03 +0530

    css done

--- my-app/src/com/Home.jsx
+++ my-app/src/com/Home.jsx
@@ -2,22 +2,42 @@ import { useState } from 'react';
 import React from 'react'
 import './Home.css';
 import Time from './Time';
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import { FaEdit } from "react-icons/fa";
+import { MdDeleteForever } from "react-icons/md";
+
+
+
 
 const Home = () => {
+  const [task, stateTask] = useState("")
+
+
+  const addTask = () => {
+    console.log("addTask")
+  }
+  const editTask = () => {
+    console.log("editTask")
+  }
+  const deleteTask = () => {
+    console.log("deleteTask")
+  }
   return (
     <React.Fragment>
       <div className='Home-main'>
         <div className='pre  bor'>
-        <div className='Home-head-time'>
-        <h1>
-        Todo APP <Time/>
-      </h1>
-      <p></p>
-        </div>
+          <div className='Home-head-time'>
+            <h1 className='Home-app'>
+              Todo APP <Time />
+            </h1>
+
+          </div>
           <input className='Home-inp' placeholder='Add your daily task'></input>
           <div className='Home-showData'></div>
           <div className='Home-btn'>
-            <button>+</button>
+            <AiOutlinePlusCircle onClick={addTask} />
+            <FaEdit onClick={editTask} />
+            <MdDeleteForever onClick={deleteTask} />
           </div>
         </div>
       </div>
